01f7319836a409a592370e587015ec9466ebba18
Samin Rahman <55238473+hornet67@users.noreply.github.com>
2026-04-01 15:23:14 +0600

    working

--- livecalculator/asgi.py
+++ livecalculator/asgi.py
@@ -1,9 +1,12 @@
 
 import os
 import django
+import myapp.routing
 from django.core.asgi import get_asgi_application
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livecalculator.settings')
 django.setup()
@@ -11,6 +14,7 @@ django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
+    "websocket":AuthMiddlewareStack(URLRouter(myapp.routing.websocket_urlpatterns))
     
     
 })
